94ea41910b22abf982b17b6c30b546f489af2e62
ESET0091 <Gopal.singh@esyasoft.com>
2025-09-04 21:34:50 +0530

    Day 2 assignment

--- Basics of Python/Day 2/main.py
+++ Basics of Python/Day 2/main.py
@@ -9,7 +9,7 @@
 # Create a list of tuples (points with x and y coordinates)
 points = [(1, 2), (5, 8), (3, 4), (7, 2), (6, 6)]
 
-# Iterate through the list of points
+# Iterate through the list of given points
 for x, y in points:
     if x > y:
         print(f"The X coordinate is greater than the Y coordinate for point ({x}, {y}).")
