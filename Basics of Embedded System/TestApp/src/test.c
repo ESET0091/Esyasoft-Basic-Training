27d0e063aea31eea22cfbb9af693ab016e45c226
ESET0091 <Gopal.singh@esyasoft.com>
2025-09-07 10:23:44 +0530

    Added push  button functionality to LED

--- Basics of Embedded System/TestApp/src/test.c
+++ Basics of Embedded System/TestApp/src/test.c
@@ -1,34 +1,32 @@
-
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED_PIN PD4     // Pin 7 on PORTD
-#define BUTTON_PIN PD1  // Pin 3 on PORTD
+#define LED_PIN PD4 // port D4 set to Led Pin
+#define PUSH_BUTTON PD5  // port D5 set to Push Button
 
-int main(void) {
-    // Set LED_PIN as output
-    DDRD |= (1 << LED_PIN);
+int main(void){
+    DDRD |= (1 << LED_PIN);  //assigning DDRD as o/p register
+    DDRD &= ~(1 << PUSH_BUTTON); //assigning DDRD as i/p register
+    PORTD |= (1 << PUSH_BUTTON); 
 
-    // Set BUTTON_PIN as input WITH pull-up resistor
-    DDRD &= ~(1 << BUTTON_PIN);
-    PORTD |= (1 << BUTTON_PIN);
 
-    while (1) {
-       if(!(PIND & (1<< BUTTON_PIN))) {
-        // Turn on LED
-        PORTD |= (1 << LED_PIN);
-        _delay_ms(500); // Keep LED on for 500 ms
+    while(1){
+     
+      if(!(PIND & (1 << PUSH_BUTTON))){
+          //LED ON 
+          PORTD |= (1<< LED_PIN);
+          //Delay 500ms
+          _delay_ms(250);
 
-        // Turn off LED
-        PORTD &= ~(1 << LED_PIN);
-        _delay_ms(500); // Keep LED off for 500 ms
-       }
-       else {
-        // Ensure LED is off when button is not pressed
-        PORTD &= ~(1 << LED_PIN);
-       }
-
+          //LED OFF
+          PORTD &= ~(1<< LED_PIN);
+          _delay_ms(250);
+      }
+      
+      else{
+          PORTD &= ~(1<< LED_PIN); //LED OFF
+      }
+      
     }
-
-    return 0; // This line will never be reached
+    return 0;
 }
