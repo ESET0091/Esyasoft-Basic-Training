a60035223844665b230ff1269d86a2c306490f3f
ESET0091 <Gopal.singh@esyasoft.com>
2025-09-11 15:59:31 +0530

    Added week2 assesment 2

---
+++ Basics_of_MSSQL/Week2_Assesment2.sql
@@ -0,0 +1,65 @@
+create database employees;
+use employees;
+
+create table users (user_id int primary key, email varchar(20), name varchar(50));
+create table books (product_id int primary key, title varchar(50), price float);
+alter table users alter column email varchar(40);
+
+select * from users;
+select * from books;
+
+
+create table orders (order_no int primary key, user_id int, product_id int, 
+ foreign key (user_id) references users(user_id), foreign key (product_id) references books(product_id));
+
+select * from users;
+select * from books;
+select * from orders;
+
+insert into users values(1, 'callmebhai@gmail.com', 'GopalBhai'),
+(2, 'toxicmajhi@gmail.com', 'Manjit'),(3,'kalajadu@gmail.com','SoumyaDeep'),
+(4,'vimalpanmasala@gmail.com','JaiSai'),(5,'ghostlaugh@gmail.com','Namitha');
+
+insert into books values(101, 'MSSQL',100),(102, 'Half Girlfriend', 699), (103,'Black Magic',700),
+ (104, 'How to Marfa', 2000), (105,'How to Laugh', 2001);
+
+insert into orders values (401,1,101), (402,2,102), (403,2,103),(404,3,103), (405,3,104), (406,4,102), (407,5,102), (408,5,105);
+select * from orders;
+
+
+
+ -- Query 1 --
+ select row_number() over ( partition by u.user_id order by price desc) as row_num, rank() over (partition by u.user_id order by price desc) as rank,
+ dense_rank() over (partition by u.user_id order by price desc) as dense_rank,
+ u.user_id, u.name, b.title, b.price from users u inner join orders o on u.user_id = o.user_id
+ inner join books b on  b.product_id= o.product_id;
+
+ select u.user_id,
+    u.name,
+    b.title,
+    b.price,
+    ROW_NUMBER() OVER (PARTITION BY u.user_id ORDER BY b.price DESC) AS RowNum,
+    RANK() OVER (PARTITION BY u.user_id ORDER BY b.price DESC) AS RankNum,
+    DENSE_RANK() OVER (PARTITION BY u.user_id ORDER BY b.price DESC) AS DenseRankNum
+FROM orders o
+JOIN users u ON o.user_id = u.user_id
+JOIN books b ON o.product_id = b.product_id;
+
+
+ -- Query 2 --
+ /-- select u.name,  sum(b.price), rank() over (partition by u.user_id order by sum(b.price) desc) as rank,
+ /-- dense_rank() over (partition by u.user_id order by sum(price) desc) as dense_rank,
+ /-- u.user_id, u.name, b.title, b.price from users u inner join orders o on u.user_id = o.user_id
+ /-- inner join books b on  b.product_id= o.product_id;
+
+ SELECT 
+    u.user_id,
+    u.name,
+    SUM(b.price) AS TotalSpent,
+    RANK() OVER (ORDER BY SUM(b.price) DESC) AS RankNum,
+    DENSE_RANK() OVER (ORDER BY SUM(b.price) DESC) AS DenseRankNum
+FROM orders o
+JOIN users u ON o.user_id = u.user_id
+JOIN books b ON o.product_id = b.product_id
+GROUP BY u.user_id, u.name;
+
