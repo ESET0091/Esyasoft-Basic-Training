2cefde011120659e5bbe4bdf8dc802e9c0e37581
ESET0091 <Gopal.singh@esyasoft.com>
2025-09-19 12:37:48 +0530

    Added class assignment 2

---
+++ Basics_of_DLMS/DLMS_GET_SET/MeterObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLMS_GET_SET
+{
+    public class MeterObject
+    {
+        public byte[] obis { get; set; }
+        public string name { get; set; }
+        public int value { get; set; }
+
+        public MeterObject(byte[] obisCode, string objectName, int initialValue)
+        {
+            obis = obisCode;
+            name = objectName;
+            value = initialValue;
+        }
+    }
+}
