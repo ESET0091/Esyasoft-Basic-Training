005facd80cbaaf2c8fbeff1eaa279f170f151156
ESET0091 <Gopal.singh@esyasoft.com>
2025-09-26 23:27:00 +0530

    Added a web application demonstrating API callls

---
+++ Basics_of_DOTNET/Stretch_Goals/Web_Api_Demo/Models/Products.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Api_Demo.Models
+{
+    public class Products
+    {
+        public int Std_id { get; set; }
+        public string Std_name { get; set; }
+        public int Std_email { get; set; }
+    }
+}
